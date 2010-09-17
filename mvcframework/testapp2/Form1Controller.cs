633316352758a1002ed4fc2abb446f3b46e31aa6
memson <memson@gmail.com>
2010-09-17 14:09:28 +0000

    Refactored the code so that the code generation is done in multiple steps and a tree of info is created. This code still doesn't handle recursive situations.
    Added a second example - this one originally used the mvctool to generate the code. More proof of concept than anything else. There is a batch file includes that will recreate the code. I've also created a text file with the code I added to it.

---
+++ mvcframework/testapp2/Form1Controller.cs
@@ -0,0 +1,66 @@
+/*Auto generated*/ 
+	
+using System; 
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//3rd Party
+using RatCow.MvcFramework;
+
+namespace testapp2
+{
+	internal partial class Form1Controller: BaseController<Form1>
+	{
+		public Form1Controller() : base()
+		{
+		}
+
+		void button1Click()
+		{
+
+		}
+
+	}
+
+
+#region GUI glue code
+
+	partial class Form1Controller
+	{
+		[Outlet("textBox1")]
+		public TextBox textBox1 { get; set; }
+		[Outlet("label1")]
+		public Label label1 { get; set; }
+		[Outlet("button1")]
+		public Button button1 { get; set; }
+		[Action("button1", "Click")]
+		public void Fbutton1_Click(object sender, EventArgs e)
+		{
+			//Auto generated call
+			button1Click();
+		}
+
+		[Outlet("checkBox1")]
+		public CheckBox checkBox1 { get; set; }
+	}
+#endregion /*GUI glue code*/
+
+}
+
+n button1 { get; set; }
+		[Action("button1", "Click")]
+		public void Fbutton1_Click(object sender, EventArgs e)
+		{
+			//Auto generated call
+			button1Click();
+		}
+
+		[Outlet("checkBox1")]
+		public CheckBox checkBox1 { get; set; }
+	}
+#endregion /*GUI glue code*/
+
+}
+
