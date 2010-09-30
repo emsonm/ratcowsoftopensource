9a9f6259e02d44998ca1e2d620ebb9ae71a79758
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2010-09-30 17:12:14 +0000

    Added the start of a class that will wrap the annoying calls to Winforms that are unavoidable. This currently handles S.W.F.MessageBox.Show()

---
+++ trunk/mvcframework/MvcFramework/UIBridge.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RatCow.MvcFramework
+{
+  public class UIBridge
+  {
+    /// <summary>
+    /// This is to save referencing the underlying OS message boxes. 
+    /// </summary>
+    /// <param name="message"></param>
+    public static void Alert(string message)
+    {
+      Alert(message, "Warning");
+    }
+
+    /// <summary>
+    /// This is to save referencing the underlying OS message boxes. 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="title"></param>
+    public static void Alert(string message, string title)
+    {
+      System.Windows.Forms.MessageBox.Show(message, title, System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning); 
+    }
+
+    /// <summary>
+    /// This is to save referencing the underlying OS message boxes. 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public static bool BooleanDecision(string message)
+    {
+      return BooleanDecision(message, "Please choose...");
+    }
+
+    /// <summary>
+    /// This is to save referencing the underlying OS message boxes. 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="title"></param>
+    /// <returns></returns>
+    public static bool BooleanDecision(string message, string title)
+    {
+      return (System.Windows.Forms.MessageBox.Show(message, title, System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes); 
+    }
+  }
+}
