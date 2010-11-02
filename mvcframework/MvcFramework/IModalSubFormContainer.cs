8691908e426e3927ed10700c825924fb36203408
memson <memson@gmail.com>
2010-11-02 13:16:43 +0000

    Added modal sub form (view) functionality. A Controller can now own and execute modal sub views.

---
+++ mvcframework/MvcFramework/IModalSubFormContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RatCow.MvcFramework
+{
+  /// <summary>
+  /// We implement this
+  /// </summary>
+  public interface IModalSubFormContainer //: BaseController<System.Windows.Forms.Form>
+  {
+    /// <summary>
+    /// Subclasses override this
+    /// </summary>
+    /// <returns></returns>
+    bool PerformModalTask();
+  }
+}
---
+++ mvcframework/MvcFramework/ModalSubFormController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RatCow.MvcFramework
+{
+  public class ModalSubFormController<T> : BaseController<T>, IModalSubFormContainer
+  {
+
+    public ModalSubFormController()
+      : base()
+    {
+    }
+
+    public virtual bool PerformModalTask()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
