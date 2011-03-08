4611c5c457b7116ae277a5c35b08bf69a212ed02
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2011-03-08 13:51:07 +0000

    Another snapshot

---
+++ tags/mvcframework/previewrelease/3/MvcFramework/IModalSubFormContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RatCow.MvcFramework
+{
+  /// <summary>
+  /// We implement this
+  /// </summary>
+  public interface IModalSubFormContainer //: BaseController<System.Windows.Forms.Form>
+  {
+    /// <summary>
+    /// Subclasses override this
+    /// </summary>
+    /// <returns></returns>
+    bool PerformModalTask();
+
+    /// <summary>
+    /// Performs a task with associated data
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    bool PerformModalTask<T>(T data);
+  }
+}
