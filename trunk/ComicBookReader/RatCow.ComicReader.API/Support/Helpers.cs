7cc4adaa7d83af01d45dc550bc5d2830bfe43185
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2011-04-06 10:38:02 +0000

    Added better support for ComicBookInfo (as in, I looked at a correctly tagged file and clocked why my initial attempts to write the data were incorrect!) The info now appears in ComicBookLover on the iPhone, but has spurious quotes... think it just doesn't need the additional escaping - so will try that next.

---
+++ trunk/ComicBookReader/RatCow.ComicReader.API/Support/Helpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RatCow.ComicReader.API
+{
+  public static class StringHelper
+  {
+    /// <summary>
+    /// Remove the quotes if they exist
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static string Unquote(this string s)
+    {
+      if (s.StartsWith("\"") && s.EndsWith("\""))
+        return s.Trim('\\', '\"');
+      else
+        return s;      
+    }
+
+    /// <summary>
+    /// add quotes
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static string Quote(this string s)
+    {
+      return String.Format("\"{0}\"", s);
+    }
+  }
+}
