4aec2dae4519e5bd7467b5162aa1cb3a2ec91c41
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2011-05-06 13:41:08 +0000

    Added a simple Hiragana encoding library. A little simplistic and a little too basic, but will be improved later on.

---
+++ trunk/hiragana-tool/RatCow.Hiragana/ElementDescription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RatCow.Hiragana
+{
+  internal class ElementDescription
+  {
+    public ElementDescription(char initial, int offset, int multiplier)
+    {
+      this.Initial = initial;
+      this.Offset = offset;
+      this.Multiplier = multiplier;
+    }
+
+    public char Initial { get; internal set; }
+    public int Offset { get; internal set; }
+    public int Multiplier { get; internal set; }
+  }
+
+  internal class ElementDescriptionList : List<ElementDescription>
+  {
+
+    public void Add(char initial, int offset, int multiplier)
+    {
+      this.Add(new ElementDescription(initial, offset, multiplier));
+    }
+  }
+}
---
+++ trunk/hiragana-tool/RatcowHiriganaTool/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using RatCow.Hiragana;
+
+namespace RatcowHiriganaTool
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      HiraganaTable h = new HiraganaTable();
+
+      textBox1.Text = h.Test(); //builds a table of characters
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      HiraganaTable h = new HiraganaTable();
+
+      textBox1.Text = String.Format("{0}{1}{2}", h.DecodeBlock("ta"), h.DecodeBlock("na"), h.DecodeBlock("ka"));
+    }
+  }
+}
