27cc7624741a9bed5d3dd1357aa17da8dc77747e
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2012-10-02 16:39:58 +0000

    Some fixes to the ModalSubFormController to remove a compilation warning (Template name collision.)
    Start of a port of the MvcFramework to WPF!! The current implementation works almost "out of the box". The WPF version of the base controller has been stripped bare, and does not include any of the Compact Framework code. It is also missing the ModalSubController and the various helpers (as these will mostly come from the RatCow.Utilities namespace.) But the hand translated test app compiles and runs. (I don't claim to be a WPF expert, so I'm sure the little hack I did to the App.xaml.cs and code in the Application_Startup is "bad".) The point is, the same controller with minimal changes works. The major change was the EventArgs becoming RoutedEventArgs, but as I plan to factor out the direct use of EventArgs style constructs, I don't see this as a biggy. Some of the generated code is still commented out, as there seems ot be no analogue to the ListView.
    
    I'm not sure there's a lot of point to all of this. The main thrust was to get the compiler to work with WPF and that is not any where near done!

--- trunk/mvcframework40/RatCow.MvcFramework.WPF/Attributes.cs
+++ trunk/mvcframework40/RatCow.MvcFramework.WPF/Attributes.cs
@@ -1,18 +1,18 @@
 ﻿/*
  * Copyright 2010 Rat Cow Software and Matt Emson. All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice, this list of
  *    conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list
  *    of conditions and the following disclaimer in the documentation and/or other materials
  *    provided with the distribution.
- * 3. Neither the name of the Rat Cow Software nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without specific prior written
+ * 3. Neither the name of the Rat Cow Software nor the names of its contributors may be used 
+ *    to endorse or promote products derived from this software without specific prior written 
  *    permission.
- *
+ *    
  * THIS SOFTWARE IS PROVIDED BY RAT COW SOFTWARE "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
@@ -26,37 +26,68 @@
  * The views and conclusions contained in the software and documentation are those of the
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of Rat Cow Software and Matt Emson.
- *
+ * 
  */
 
 using System;
 using System.Collections.Generic;
+#if !CF_20
 using System.Linq;
+#endif
 using System.Text;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace RatCow.MvcFramework
+namespace RatCow.MvcFramework.WPF
 {
-  public class ModalSubFormController<T> : BaseController<T>, IModalSubFormContainer
-  {
-    public ModalSubFormController()
-      : base()
+    public class ViewAttribute : Attribute
     {
+        public ViewAttribute()
+        {
+        }
     }
 
-    public virtual bool PerformModalTask()
+    public class OutletAttribute: Attribute
     {
-      throw new NotImplementedException();
+        public string Name {get; private set;}
+
+        public OutletAttribute(string name)
+        {
+            Name = name;
+        }
     }
 
-    public bool PerformModalTask<D>( D data )
+    public class ActionAttribute: Attribute
     {
-      throw new NotImplementedException();
-    }
+        public string Name {get; private set;}
+        public string Action { get; private set; }
+        public Type EventType { get; private set; }
 
-    public bool PerformModalTask<D, R>( D data, ref R result )
-    {
-      throw new NotImplementedException();
-    }
+        public ActionAttribute(string name, string action)
+        {
+            Name = name;
+            Action = action;
+            EventType = null; //default to the type of the underlying event... typeof(EventHandler);
+        }
 
-  }
+        public ActionAttribute(string name, string action, Type eventType)
+        {
+          Name = name;
+          Action = action;
+          EventType = eventType; //overrides the event type (experimental)
+        }
+
+
+
+
+    }
+    
 }
---
+++ trunk/mvcframework40/RatCow.MvcFramework.WPF/BaseController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace RatCow.MvcFramework.WPF
+{
+  public class BaseController<T>: IDisposable
+  {
+
+    protected T target;
+
+    /// <summary>
+    /// required internally
+    /// </summary>
+    /// <returns></returns>
+    private static Type GetTargetType()
+    {
+      Type t = typeof( T );
+      return t;
+    }
+
+    public BaseController()
+    {
+      //Create new instance of T
+      Type t = typeof( T );
+      target = (T)Activator.CreateInstance( t );
+
+      //attach the outlets and actions
+      FixUp();
+      FixUpView();
+    }
+
+    public BaseController( T aTarget )
+    {
+      target = aTarget;
+
+      //attach the outlets and actions
+      FixUp();
+      FixUpView();
+    }
+
+    private void FixUpView()
+    {
+      //throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// This will build all of the internl look-up structures
+    /// </summary>
+    protected virtual void FixUp()
+    {
+      Type t = typeof( T );
+      Type self = this.GetType();
+
+      //look for attributes
+
+      BindingFlags bindingFlags = ( BindingFlags.Instance | BindingFlags.GetField | BindingFlags.GetProperty | BindingFlags.SetField | BindingFlags.SetProperty | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.FlattenHierarchy );
+
+      //search for all Outlets
+      PropertyInfo[] pia = self.GetProperties();
+      foreach ( var pi in pia )
+      {
+        OutletAttribute[] outlets = (OutletAttribute[])( pi.GetCustomAttributes( typeof( OutletAttribute ), true ) );
+        foreach ( var outlet in outlets )
+        {
+          //do something
+          FieldInfo fi = t.GetField( outlet.Name, bindingFlags );
+
+          if ( fi != null )
+          {
+            object value = fi.GetValue( target ); //this is the value pointing to the control to hook
+
+            pi.SetValue( this, value, null );
+
+            //search for all Actions
+            MethodInfo[] mia = self.GetMethods(); // might need to revisit binding flags?
+            foreach ( var mi in mia )
+            {
+              ActionAttribute[] actions = (ActionAttribute[])( mi.GetCustomAttributes( typeof( ActionAttribute ), true ) );
+              foreach ( var action in actions )
+              {
+                if ( action.Name == outlet.Name )
+                {
+                  Type ct = fi.FieldType;
+                  //do something
+                  EventInfo ei = ct.GetEvent( action.Action, bindingFlags );
+                  if ( ei != null )
+                  {
+                    Type evt = ( action.EventType == null ? ei.EventHandlerType : action.EventType );
+                    Delegate temp = Delegate.CreateDelegate( evt, this, mi, false );
+                    ei.AddEventHandler( value, temp );
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    public T View { get { return target; } }
+
+
+    #region IDisposable Members
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
