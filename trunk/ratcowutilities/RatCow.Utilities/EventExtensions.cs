ac62542fab3c2313c4b114c842c9ef9973f70965
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2013-04-18 14:48:03 +0000

    Ported some changes made elsewhere in to main branch.

---
+++ trunk/ratcowutilities/RatCow.Utilities/EventExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace RatCow.Utilities
+{
+  /// <summary>
+  /// See : http://www.waytocoding.com/2011/04/how-to-remove-all-event-handlers-from.html
+  /// </summary>
+  public static class EventExtension
+  {
+    public static void RemoveEvents<T>( this Control aTarget, string aEvent )
+    {
+      FieldInfo fi = typeof( Control ).GetField( aEvent, BindingFlags.Static | BindingFlags.NonPublic );
+      if ( fi != null )
+      {
+        object obj = fi.GetValue( aTarget.CastTo<T>() );
+        PropertyInfo pi = aTarget.CastTo<T>().GetType().GetProperty( "Events",
+            BindingFlags.NonPublic | BindingFlags.Instance );
+        EventHandlerList list = (EventHandlerList)pi.GetValue( aTarget.CastTo<T>(), null );
+        list.RemoveHandler( obj, list[ obj ] );
+      }
+    }
+  }
+}
