158c89dd55d5cc22964fe4404ca2427457adecad
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2013-04-18 14:41:42 +0000

    Added a capcha class borrowed from CodeProject... was in VB originally, converted to C# and fixed a couple of bugs (passing byref rather than byval and scoping of floats.) Also added a "Refresh" method to make it more useful to me.

---
+++ trunk/ratcowutilities/TestCapacha/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestCapacha
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    RatCow.Utilities.CaptchaImage capatcha = new RatCow.Utilities.CaptchaImage();
+    string legend = String.Empty;
+
+    private void Form1_Load( object sender, EventArgs e )
+    {
+      Generate();
+    }
+
+    private void Generate()
+    {
+      capatcha.Refresh();
+      legend = capatcha.Text;
+      pictureBox1.Image = capatcha.RenderImage();
+    }
+
+    private void button1_Click( object sender, EventArgs e )
+    {
+      if ( String.Compare( textBox1.Text, legend, true ) == 0 )
+      {
+        MessageBox.Show( "Match!" );
+        Generate();
+      }
+      else MessageBox.Show( "Failed!" );
+    }
+  }
+}
