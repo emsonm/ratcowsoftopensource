d2ab4810e6258c8cee1af6941160c09d89bac4d8
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2013-05-02 09:12:11 +0000

    Added FileShares with a single method to create a runtime file share on a remote file system.

---
+++ trunk/ratcowutilities/RatCow.Utilities/FileShares.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RatCow.Utilities
+{
+  public class FileShares
+  {
+    /// <summary>
+    /// The calling process must retain a reference to the process whilst it is using the share
+    /// </summary>
+    public static Process CreateShare( string sharePath, string userName, string password )
+    {
+      var process = new Process();
+
+      process.StartInfo.UseShellExecute = false;
+      process.StartInfo.CreateNoWindow = true;
+      process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      process.StartInfo.FileName = "net.exe";
+      process.StartInfo.Arguments = String.Format( @"use {0} {1} /USER:{2}", sharePath, password, userName );
+      process.StartInfo.RedirectStandardError = false;
+      process.StartInfo.RedirectStandardOutput = false;
+      process.StartInfo.UseShellExecute = false;
+      process.StartInfo.CreateNoWindow = true;
+
+      process.Start();
+      process.WaitForExit();
+      return process;
+    }
+  }
+}
