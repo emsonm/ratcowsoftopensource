189333a85747f1d209b94611083cab660bbac582
memson <memson@gmail.com>
2013-06-23 02:48:34 +0000

    A couple of simple custom WPF controls... more of a learning experience than something overly useful!

---
+++ ratcowutilities/RatCow.WPF.Controls/RatCow.WPF.Controls/CheckListBox.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace RatCow.WPF.Controls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RatCow.WPF.Controls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RatCow.WPF.Controls;assembly=RatCow.WPF.Controls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CheckListBox/>
+    ///
+    /// </summary>
+    public class CheckListBox : UserControl
+    {
+        static CheckListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckListBox), new FrameworkPropertyMetadata(typeof(CheckListBox)));
+
+           
+        }
+
+        public CheckListBox()
+            : base()
+        {
+            Items = new ObservableCollection<CheckListItem>();
+        }
+  
+
+
+        public ObservableCollection<CheckListItem> Items
+        {
+            get { return (ObservableCollection<CheckListItem>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ObservableCollection<CheckListItem>), typeof(CheckListBox), new UIPropertyMetadata(null));
+
+
+    }
+
+    public class CheckListItem : DependencyObject
+    {
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(CheckListItem), new UIPropertyMetadata(null));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(CheckListItem), new UIPropertyMetadata(null));
+
+        public string Key
+        {
+            get { return (string)GetValue(KeyProperty); }
+            set { SetValue(KeyProperty, value); }
+        }
+
+        public static readonly DependencyProperty KeyProperty =
+            DependencyProperty.Register("Key", typeof(string), typeof(CheckListItem), new UIPropertyMetadata(null));
+    }
+}
---
+++ ratcowutilities/RatCow.WPF.Controls/TestBed/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace TestBed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            chx.Items = data;
+        }
+
+        ObservableCollection<RatCow.WPF.Controls.CheckListItem> data = new ObservableCollection<RatCow.WPF.Controls.CheckListItem>() {
+            new RatCow.WPF.Controls.CheckListItem() { Text = "Hello", IsChecked=true },
+            new RatCow.WPF.Controls.CheckListItem() { Text = "There", IsChecked=false },
+            new RatCow.WPF.Controls.CheckListItem() { Text = "This", IsChecked=true },
+            new RatCow.WPF.Controls.CheckListItem() { Text = "Is", IsChecked=false },
+            new RatCow.WPF.Controls.CheckListItem() { Text = "Test", IsChecked=true },
+            new RatCow.WPF.Controls.CheckListItem() { Text = "Data", IsChecked=false },
+        };
+
+        class X
+        {
+            public bool Value { get; set; }
+            public string Text { get; set; }
+        }
+
+        private void HeaderControl_ImageClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Yes it works");
+        }
+    }
+}
