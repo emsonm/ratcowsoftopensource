e535b560e4a94a0b02e5d3e8d1d9769d7d17402d
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2013-08-02 09:46:18 +0000

    Added a dialog to change colour details.

---
+++ trunk/Colorchart/Colorchart/ColorChangeDialog.Designer.cs
@@ -0,0 +1,188 @@
+﻿/*
+ * Copyright 2013 Rat Cow Software and Matt Emson. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ *    conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *    of conditions and the following disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ * 3. Neither the name of the Rat Cow Software nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RAT COW SOFTWARE "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Rat Cow Software and Matt Emson.
+ *
+ */
+
+namespace RatCow.Colorchart
+{
+    partial class ColorChangeDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.rLabel = new System.Windows.Forms.Label();
+            this.gLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.shadeControl = new System.Windows.Forms.VScrollBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.closeButton.Location = new System.Drawing.Point(247, 121);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorPanel.Location = new System.Drawing.Point(12, 12);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(80, 80);
+            this.colorPanel.TabIndex = 1;
+            this.colorPanel.Click += new System.EventHandler(this.colorPanel_Click);
+            // 
+            // rLabel
+            // 
+            this.rLabel.AutoSize = true;
+            this.rLabel.Location = new System.Drawing.Point(109, 22);
+            this.rLabel.Name = "rLabel";
+            this.rLabel.Size = new System.Drawing.Size(16, 13);
+            this.rLabel.TabIndex = 2;
+            this.rLabel.Text = "...";
+            this.rLabel.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // gLabel
+            // 
+            this.gLabel.AutoSize = true;
+            this.gLabel.Location = new System.Drawing.Point(109, 43);
+            this.gLabel.Name = "gLabel";
+            this.gLabel.Size = new System.Drawing.Size(16, 13);
+            this.gLabel.TabIndex = 3;
+            this.gLabel.Text = "...";
+            this.gLabel.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(109, 64);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(16, 13);
+            this.bLabel.TabIndex = 4;
+            this.bLabel.Text = "...";
+            this.bLabel.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // shadeControl
+            // 
+            this.shadeControl.Location = new System.Drawing.Point(209, 15);
+            this.shadeControl.Maximum = 255;
+            this.shadeControl.Name = "shadeControl";
+            this.shadeControl.Size = new System.Drawing.Size(17, 80);
+            this.shadeControl.TabIndex = 5;
+            this.shadeControl.ValueChanged += new System.EventHandler(this.shadeControl_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lighter";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(244, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Darker";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // ColorChangeDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.closeButton;
+            this.ClientSize = new System.Drawing.Size(328, 156);
+            this.ControlBox = false;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.shadeControl);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.gLabel);
+            this.Controls.Add(this.rLabel);
+            this.Controls.Add(this.colorPanel);
+            this.Controls.Add(this.closeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ColorChangeDialog";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Alter color";
+            this.Load += new System.EventHandler(this.ColorChangeDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.Label rLabel;
+        private System.Windows.Forms.Label gLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.VScrollBar shadeControl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ trunk/Colorchart/Colorchart/ColorChangeDialog.cs
@@ -0,0 +1,180 @@
+﻿/*
+ * Copyright 2013 Rat Cow Software and Matt Emson. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ *    conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *    of conditions and the following disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ * 3. Neither the name of the Rat Cow Software nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RAT COW SOFTWARE "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Rat Cow Software and Matt Emson.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RatCow.Colorchart
+{
+    public partial class ColorChangeDialog : Form
+    {
+        #region Fields
+
+        enum TargetColorElement { R, G, B };
+
+        Color _startColor;
+        Color _currentColor;
+        TargetColorElement _targetColorElement = TargetColorElement.R;
+        Font defaultFont = new Font("Arial", 9);
+        Font selectedFont = new Font("Arial", 9, FontStyle.Bold);
+
+        #endregion
+
+        #region Constructor
+
+        public ColorChangeDialog()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void SetUI()
+        {
+            colorPanel.BackColor = _currentColor;
+            rLabel.Text = String.Format("R : {0}", _currentColor.R);
+            gLabel.Text = String.Format("G : {0}", _currentColor.G);
+            bLabel.Text = String.Format("B : {0}", _currentColor.B);
+
+            rLabel.Font = (_targetColorElement == TargetColorElement.R ? selectedFont : defaultFont);
+            gLabel.Font = (_targetColorElement == TargetColorElement.G ? selectedFont : defaultFont);
+            bLabel.Font = (_targetColorElement == TargetColorElement.B ? selectedFont : defaultFont);
+
+            switch (_targetColorElement)
+            {
+                case TargetColorElement.R:
+                    shadeControl.Value = _currentColor.R;
+                    break;
+
+                case TargetColorElement.G:
+                    shadeControl.Value = _currentColor.G;
+                    break;
+
+                case TargetColorElement.B:
+                    shadeControl.Value = _currentColor.B;
+                    break;
+            }
+        }
+
+
+        public static Color Show(Color startColor)
+        {
+            using (var dialog = new ColorChangeDialog())
+            {
+                dialog._startColor = startColor;
+                dialog._currentColor = startColor;
+
+                dialog.ShowDialog();
+
+                return dialog._currentColor;
+            }
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private void ColorChangeDialog_Load(object sender, EventArgs e)
+        {
+            SetUI();
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            if (sender == rLabel)
+            {
+                _targetColorElement = TargetColorElement.R;
+            }
+            else if (sender == gLabel)
+            {
+                _targetColorElement = TargetColorElement.G;
+            }
+            else if (sender == bLabel)
+            {
+                _targetColorElement = TargetColorElement.B;
+            }
+
+            SetUI();
+        }
+
+        private void shadeControl_ValueChanged(object sender, EventArgs e)
+        {
+            switch (_targetColorElement)
+            {
+                case TargetColorElement.R:
+                    _currentColor = Color.FromArgb(shadeControl.Value, _currentColor.G, _currentColor.B);
+                    break;
+
+                case TargetColorElement.G:
+                    _currentColor = Color.FromArgb(_currentColor.R, shadeControl.Value, _currentColor.B);
+                    break;
+
+                case TargetColorElement.B:
+                    _currentColor = Color.FromArgb(_currentColor.R, _currentColor.G, shadeControl.Value);
+                    break;
+            }
+
+            SetUI();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            _currentColor = Color.FromArgb(_currentColor.R + 10 >= 255 ? 255 : _currentColor.R + 10, _currentColor.G + 10 >= 255 ? 255 : _currentColor.G + 10, _currentColor.B + 10 >= 255 ? 255 : _currentColor.B + 10);
+
+            SetUI();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            _currentColor = Color.FromArgb(_currentColor.R - 10 <= 0 ? 0 : _currentColor.R - 10, _currentColor.G - 10 <= 0 ? 0 : _currentColor.G - 10, _currentColor.B - 10 <= 0 ? 0 : _currentColor.B - 10);
+
+
+            SetUI();
+        }
+
+        private void colorPanel_Click(object sender, EventArgs e)
+        {
+            _currentColor = _startColor;
+
+            SetUI();
+        }
+
+        #endregion
+    }
+}
