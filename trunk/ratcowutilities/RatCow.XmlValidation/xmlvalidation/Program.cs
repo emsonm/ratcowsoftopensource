75c730b7e54c78cb6b6460a0a29239858f1ef221
memson@gmail.com <memson@gmail.com@079d2e2a-948e-11de-9a8c-5f207592b8ba>
2013-08-15 11:11:05 +0000

    Added a simple report in html

--- trunk/ratcowutilities/RatCow.XmlValidation/xmlvalidation/Program.cs
+++ trunk/ratcowutilities/RatCow.XmlValidation/xmlvalidation/Program.cs
@@ -9,8 +9,24 @@ namespace RatCow.XmlValidation
     {
         static void Main(string[] args)
         {
+            bool isHelp = args.Select(x => (x.StartsWith("/") || x.StartsWith("-")) && (x.EndsWith("?") || x.ToUpper().EndsWith("H"))).Contains(true);
+            if (args.Length < 2 || args.Contains("/?") || isHelp)
+            {
+                Console.WriteLine("Useage: xmlvalidation xmlfileOrPath xsdFile [options] ");
+                Console.WriteLine("/? or /H - show this help");
+                Console.WriteLine("/F:????  - to specify a different file extension");
+                return;
+            }
+
+            string extension = "xml";
+            bool useExtension = args.Select(x => (x.StartsWith("/") || x.StartsWith("-")) && (x.Length > 4 && x.ToUpper().Substring(1).StartsWith("F:"))).Contains(true);
+            if (useExtension)
+            {
+                extension = args.Where(x => (x.StartsWith("/") || x.StartsWith("-")) && (x.Length > 4 && x.ToUpper().Substring(1).StartsWith("F:"))).FirstOrDefault().Substring(3);
+            }
+
             string[] errors;
-            if (!XmlValidator.Validate(args[0], args[1], out errors))
+            if (!XmlValidator.Validate(args[0], args[1], extension, out errors))
             {
                 foreach (var error in errors)
                 {
